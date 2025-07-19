0c6424dd1b9483b287ec1752e041a097e58688e2
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-07-19 15:57:23 +0530

    introduction , datatypes

---
+++ basic/basic1.js
@@ -0,0 +1,41 @@
+// console.log("Hello world...");
+// console.info("Information function....");
+// console.warn("Warning function....");
+// console.error("Error function....");
+
+
+// Datatype : number , string , boolean , undefined , null
+
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+
+b = 12.55;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+
+str = "ram";
+console.log("value of str = ",str);
+console.log("type of str = ",typeof(str));
+
+isMarried = true;
+console.log("value of isMarried = ",isMarried);
+console.log("type of isMarried = ",typeof isMarried);
+
+let x;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+let y = null;
+console.log("value of y = ",y);
+console.log("type of y = ",typeof y);
+
+
+
+abc = 16;
+console.log("value of abc = ",abc);
+console.log("value of abc = " + abc);
+
