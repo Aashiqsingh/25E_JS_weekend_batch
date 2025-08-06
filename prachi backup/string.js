551443a4e8c09a293a206de1acd7c2b14b2abd4b
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-06 17:49:35 +0530

    string

---
+++ prachi backup/string.js
@@ -0,0 +1,64 @@
+var str = " Royal technosoft p ltd.";
+
+// console.log(str);
+// console.log(str.length);
+
+
+// let x = str.charAt(0)
+// console.log(x);
+
+// let x = str.charCodeAt(0)
+// console.log(x);
+
+// let x = str.endsWith("d.")
+// let x = str.startsWith(" R")
+// let x = str.includes("soft")
+
+// console.log(x);
+
+// let x = str.lastIndexOf("o")
+// console.log(x);
+
+
+// let x = str.repeat(3)
+// console.log(x);
+
+// let x = str.replace("soft","hard")
+// let x = str.replace("o","z")
+// let x = str.replaceAll("o","z")
+
+
+// console.log(x);
+
+var str = "        Royal technosoft p ltd.        ";
+console.log(str.length);
+
+// let x = str.trim()
+// let x = str.trimEnd()
+let x = str.trimStart()
+
+
+console.log(x.length);
+
+
+
+// let x = str.slice(1)
+// let x = str.slice(1,4)
+// let x = str.slice(-1)
+// let x = str.slice(-4,-1)
+
+// console.log(x);
+
+// console.log(str.toUpperCase())
+// console.log(str.toLocaleLowerCase())
+
+// let x = str.split(" ")
+// let x = str.split("o")
+// let x = str.split("")
+
+// console.log(x);
+
+// for(let i=0;i<x.length;i++){
+//     console.log(x[i]);
+    
+// }
